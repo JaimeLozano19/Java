49a7e95b47c858f5d24644e04bbb737b8b800bca
Jaime Oswaldo Lozano García <142168151+JaimeLozano19@users.noreply.github.com>
2023-08-13 11:44:00 -0500

    Add files via upload
    Java View Example: Dive into the world of graphical user interfaces with this Java project showcasing a dynamic and interactive view. Explore the art of crafting user-friendly interfaces that seamlessly bridge functionality and aesthetics. Witness Java's prowess in creating engaging visuals that enhance user experience and interaction

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+package Vista;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        frmConvocatoria vista = new frmConvocatoria("Hilos");
+        vista.setVisible(true);
+        vista.setLocationRelativeTo(null);
+    }
+}
